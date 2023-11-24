3955fe2daaf1b42fed623412183dc8397c5092df
coryrylan <2021067+coryrylan@users.noreply.github.com>
2023-11-23 22:20:57 -0600

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import 'web-component-essentials';
 
 @Component({
   standalone: true,
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import 'zone.js';
-import('https://esm.sh/web-component-essentials@0.0.4');
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent);
