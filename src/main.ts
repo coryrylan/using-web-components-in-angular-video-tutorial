59ade413c3e404e42665ebe539fb922c5bd64139
coryrylan <2021067+coryrylan@users.noreply.github.com>
2023-11-23 22:18:18 -0600

    2 files modified

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,6 @@
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import 'zone.js';
+import { AppComponent } from './app/app.component';
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
-  `,
-})
-export class App {
-  name = 'Angular';
-}
-
-bootstrapApplication(App);
+bootstrapApplication(AppComponent);
